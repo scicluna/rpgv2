fc40d0f7a5d085b88415c3086f743d0d45705d47
scicluna <88948700+scicluna@users.noreply.github.com>
2023-04-20 11:38:30 -0400

    fix

--- src/assets/libraries/formbackgrounds.ts
+++ src/assets/libraries/formbackgrounds.ts
@@ -1,8 +1,8 @@
-import form0 from '../assets/backgrounds/form0.webp'
-import form1 from '../assets/backgrounds/form1.webp'
-import form2 from '../assets/backgrounds/form2.webp'
-import form3 from '../assets/backgrounds/form3.webp'
-import form4 from '../assets/backgrounds/form4.webp'
-import form5 from '../assets/backgrounds/form5.webp'
+import form0 from '../../assets/backgrounds/form0.webp'
+import form1 from '../../assets/backgrounds/form1.webp'
+import form2 from '../../assets/backgrounds/form2.webp'
+import form3 from '../../assets/backgrounds/form3.webp'
+import form4 from '../../assets/backgrounds/form4.webp'
+import form5 from '../../assets/backgrounds/form5.webp'
 
 export const formBackgrounds = [form0, form1, form2, form3, form4, form5]
